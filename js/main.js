246f93b1a9b571c3ed2bc1e738a12ee79de58e10
Slava <Anacondazx@rambler.ru>
2019-12-29 01:33:10 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,51 @@
+
+var string = "Hello!";
+var boolean = true;
+var sym = Symbol();
+var boolean = true;
+
+var obj = {};
+
+
+
+console.log(4/0);
+console.log('string'*9);
+//let something;
+//console.log(something)
+
+//let persone = {
+//name:"John", 
+    //Age: 25,
+    //isMarried:false
+
+//};
+//console.log(persone["name"]);
+
+
+
+ //let answer = confirm("are you here");
+ //console.log(answer);
+
+ //let answer = prompt("how are age?", "да");
+ //console.log(typeof(answer));
+
+
+ //console.log.("arr"  +  " - object");
+
+
+//let incr = 10,
+    //decr =  10;
+
+
+
+//console.log(incr++);
+//console.log(decr--);
+
+//console.log(5%2);
+
+//console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
